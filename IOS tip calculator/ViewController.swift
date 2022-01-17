00e0478b90b42f04546ec78297231373f4ca6af9
zinebtakafi <70168418+zinebtakafi@users.noreply.github.com>
2022-01-17 16:55:34 -0500

    Initial Commit

---
+++ IOS tip calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS tip calculator
+//
+//  Created by Zineb Takafi on 1/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
